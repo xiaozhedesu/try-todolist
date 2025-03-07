152686b6cc3ac216cca207b5ee987a4972d80060
xiaozhenano <xiaozhedesu0219@outlook.com>
2025-03-07 17:47:50 +0800

    跟着视频做完项目

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # try-todolist
 
-这是一个vue2练习项目，**并非原创**，来源：[20分钟学会Vue综合案例 快速巩固复习Vue语法 大学生暑假必学前端编程项目](https://www.bilibili.com/video/BV1714y167Sn/?spm_id_from=333.1007.top_right_bar_window_history.content.click&vd_source=b9ad880f6d5b2c87a5e2200233689d26)
+## 简介
+
+这是一个vue2练习项目，**并非原创**。
+
+> 视频来源：[20分钟学会Vue综合案例 快速巩固复习Vue语法 大学生暑假必学前端编程项目](https://www.bilibili.com/video/BV1714y167Sn/?spm_id_from=333.1007.top_right_bar_window_history.content.click&vd_source=b9ad880f6d5b2c87a5e2200233689d26)
+> 使用资源： [todomvc-app-template](https://github.com/tastejs/todomvc-app-template/tree/main)
+
+## 过程记录
+
+### 步骤
+
+0. 初始化一个vue2项目，然后把关于todoMVC的模版html和css导入`App.vue`文件中，准备编写功能代码。
+1. 创建一个响应式数据`todos`，用来存放待办事项。
+2. 在html部分中，对`li`使用`v-for`遍历`todos`数据用来展示。
+3. 在`methods`里实现`toggleAll(event)`达到点击控件时实现全选/全不选的效果。
+4. 对于`footer`中的分类展示，在生命钩子添加一个监听器，当URL的哈希值发生变化时调用`onHashChange()`函数（因为分类是通过URL的哈希值来决定显示的类别的）。
+5. 添加一个响应式数据`visibility`，用来储存当前需要展示的待办事项类别，并实现`onHashChange()`函数，这个函数用来根据URL的哈希值改变`visibility`。
+6. 添加一个计算属性`filteredTodos`, 保存根据`visibility`的状态筛选后的数组，并将展示页面遍历`todos`改为遍历`filteredTodos`。
+7. 在`footer`里的`filters`无序列表，添加动态类名来达到切换页面改变显示样式的效果。
+8. 添加一个计算属性`remaining`，表示列表中未勾选的项目个数，然后在页面中显示。
+9. 实现一个函数`addTodo(event)`，实现通过输入框新增数据，然后在输入框上使用`keyup.enter`监听回车来决定添加文件的时机。
+10. 实现一个函数`removeTodo(todo)`，实现删除某个待办项，然后通过`click`监听实现效果。
+11. 实现一个`clearCompleted()`，实现删除所有完成项，然后通过`click`监听实现效果。
+
+### 总结
+视频以「保存并显示待办项（1-2） -> 批量改变选择状态（3）-> 分类展示待办项（4-7） -> 其他小功能（8-11）」的顺序带领学习者学习vue2的功能特性。能流畅做完并理解这个项目，或许能说明学习者已经初步掌握vue2的基础知识。
--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,146 @@
-<script setup>
-export default {
-
-}
-</script>
-
 <template>
   <section class="todoapp">
+
     <header class="header">
-      <h1>todos</h1>
-      <input class="new-todo" placeholder="What needs to be done?" autofocus>
+      <h1>待办事项</h1>
+      <input @keyup.enter="addTodo" class="new-todo" placeholder="需要做什么？" autofocus>
     </header>
-    <!-- This section should be hidden by default and shown when there are todos -->
+
+    <!-- 这个部分应该默认隐藏，并且在有代办事项（todos）时显示 -->
     <section class="main">
-      <input id="toggle-all" class="toggle-all" type="checkbox">
+      <input id="toggle-all" class="toggle-all" type="checkbox" @click="toggleAll">
       <label for="toggle-all">Mark all as complete</label>
       <ul class="todo-list">
-        <!-- These are here just to show the structure of the list items -->
-        <!-- List items should get the class `editing` when editing and `completed` when marked as completed -->
-        <li class="completed">
+        <!-- 当处于编辑状态时，列表项应该添加 `editing` 类，当被标记为已完成时，应该添加 `completed` 类 -->
+        <li v-for="todo in filteredTodos" :key="todo.id" :class="{ completed: todo.completed }">
           <div class="view">
-            <input class="toggle" type="checkbox" checked>
-            <label>Taste JavaScript</label>
-            <button class="destroy"></button>
+            <!-- 使用 `v-model` 进行双向绑定，搭配动态绑定类名可以达到联动效果 -->
+            <input class="toggle" type="checkbox" v-model="todo.completed">
+            <label>{{ todo.title }}</label>
+            <button class="destroy" @click="removeTodo(todo)"></button>
           </div>
           <input class="edit" value="Create a TodoMVC template">
         </li>
-        <li>
-          <div class="view">
-            <input class="toggle" type="checkbox">
-            <label>Buy a unicorn</label>
-            <button class="destroy"></button>
-          </div>
-          <input class="edit" value="Rule the web">
-        </li>
       </ul>
     </section>
-    <!-- This footer should be hidden by default and shown when there are todos -->
-    <footer class="footer">
-      <!-- This should be `0 items left` by default -->
-      <span class="todo-count"><strong>0</strong> item left</span>
-      <!-- Remove this if you don't implement routing -->
+
+    <!-- 这个底部栏应该默认隐藏，并且在有代办事项（todos）时显示 -->
+    <footer v-show="todos.length != 0" class="footer">
+      <!-- 这里默认应该显示为 `0 项剩余` -->
+      <span class="todo-count">
+        <strong> {{ remaining }}</strong>
+        项剩余
+      </span>
+      <!-- 如果你没有实现URL的哈希值功能，就移除这部分 -->
       <ul class="filters">
         <li>
-          <a class="selected" href="#/">All</a>
+          <a :class="{ selected: visibility === '' }" href="#/">所有</a>
         </li>
         <li>
-          <a href="#/active">Active</a>
+          <a :class="{ selected: visibility === 'active' }" href="#/active">未完成</a>
         </li>
         <li>
-          <a href="#/completed">Completed</a>
+          <a :class="{ selected: visibility === 'completed' }" href="#/completed">已完成</a>
         </li>
       </ul>
-      <!-- Hidden if no completed items are left ↓ -->
-      <button class="clear-completed">Clear completed</button>
+      <!-- 如果没有已完成的项，则隐藏这部分 ↓ -->
+      <button class="clear-completed" @click="clearCompleted">清除已完成</button>
     </footer>
   </section>
 </template>
 
 <style scoped>
+/* 只是导入外部样式，本项目主要内容不在此 */
 @import "https://unpkg.com/todomvc-app-css@2.4.1/index.css";
 </style>
+
+<script>
+const hashStatus = [
+  '',           // 全部
+  'active',     // 未完成
+  'completed'   // 已完成
+];
+
+export default {
+  data() {
+    return {
+      // 显示在页面中的待办项列表，可以通过 `addTodo` 函数添加项，以及 `removeTodo` 函数删除项，且显示内容会动态变化
+      todos: [
+        {
+          id: 114,
+          title: '出发游玩舞萌DX',
+          completed: false
+        },
+        {
+          id: 514,
+          title: '在家游玩Arcaea',
+          completed: true
+        }
+      ],
+      // 表示当前URL的哈希值
+      visibility: ''
+    }
+  },
+  methods: {
+    // 实现全选/全不选的操作
+    toggleAll(event) {
+      this.todos.forEach(todo => todo.completed = event.target.checked);
+    },
+    // 判断URL的哈希值，当URL的哈希值变化时改变需要激活的元素
+    onHashChange() {
+      const hash = window.location.hash.replace(/#\/?/, '');  // 匹配 `#/` 并置换为空
+      if (hashStatus.includes(hash)) {
+        this.visibility = hash;
+      } else {
+        // 默认访问主页
+        window.location.hash = ''
+        this.visibility = '';
+      }
+    },
+    // 添加todo
+    addTodo(event) {
+      const title = event.target.value.trim();  // trim() 去除空格
+      if (!title) {
+        return;
+      }
+      this.todos.push({
+        id: Date.now(),   // 使用时间戳可以保证id不重复，最好使用随机值代替
+        title,
+        completed: false
+      })
+      event.target.value = ''
+    },
+    // 移除单个todo
+    removeTodo(todo) {
+      this.todos.splice(this.todos.indexOf(todo), 1);
+    },
+    // 清除已完成
+    clearCompleted() {
+      this.todos = this.todos.filter(todo => !todo.completed)
+    }
+  },
+  computed: {
+    // 根据URL的哈希值提供的筛选条件返回一个被筛选的todos列表
+    filteredTodos() {
+      switch (this.visibility) {
+        case '':
+          return this.todos
+        case 'active':
+          return this.todos.filter(todo => !todo.completed);
+        case 'completed':
+          return this.todos.filter(todo => todo.completed);
+      }
+    },
+    // 未完成项目的个数
+    remaining() {
+      return this.todos.filter(todo => !todo.completed).length;
+    }
+  },
+  mounted() {
+    // 添加监听器，当URL的哈希值改变时执行特定函数
+    window.addEventListener('hashchange', this.onHashChange);
+    // 主动调用一次，为了避免刷新后的显示问题。
+    this.onHashChange();
+  }
+}
+</script>
